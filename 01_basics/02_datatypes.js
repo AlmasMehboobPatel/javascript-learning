ca925f2f2679c6cefbecbc0aa24e01bce5c2a33a
AlmasMehboobPatel <156776699+AlmasMehboobPatel@users.noreply.github.com>
2024-02-19 11:51:48 +0000

    conversionoperation

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,7 +2,7 @@
 
 // alert(3 + 3) //we are using node js not browser
 
-// console.log(3 + 3); 
+// console.log(3 + 3); (real world coding practice)
 // console.log("Almas")
 
 
---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score = "33abc";
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let almas = Number(score)
+//console.log(typeof almas)
+//console.log(almas)
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let someNumber = 11
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
