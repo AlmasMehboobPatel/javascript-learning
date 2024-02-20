bf9794caa0a312cbae885f272c4e4be228bd8686
AlmasMehboobPatel <156776699+AlmasMehboobPatel@users.noreply.github.com>
2024-02-20 06:28:17 +0000

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -14,5 +14,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 11
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//********* Operations ******** */
+
+let value = 33
+let negValue = -value
+//console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+
+let str1 = "Hello"
+let str2 = " Almas"
+let str3 = str1 + str2
+//console.log (str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(2 + 1 + "2")
+
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+gameCounter++ // postfix 
+// presidence operator ++gameCounter
+console.log(gameCounter)
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
