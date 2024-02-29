da088a2b544c87e5ea96fb82d37c0a437929a821
AlmasMehboobPatel <156776699+AlmasMehboobPatel@users.noreply.github.com>
2024-02-29 11:26:58 +0000

    datatype summary

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,21 +1,21 @@
 let score = "33abc";
-//console.log(typeof score)
-//console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let almas = Number(score)
-//console.log(typeof almas)
-//console.log(almas)
+// console.log(typeof almas)
+// console.log(almas)
 
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 let someNumber = 11
 
 let stringNumber = String(someNumber)
-//console.log(stringNumber);
-//console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 
@@ -23,7 +23,7 @@ let stringNumber = String(someNumber)
 
 let value = 33
 let negValue = -value
-//console.log(negValue)
+// console.log(negValue)
 
 
 // console.log(2+2)
@@ -35,7 +35,7 @@ let negValue = -value
 let str1 = "Hello"
 let str2 = " Almas"
 let str3 = str1 + str2
-//console.log (str3)
+// console.log (str3)
 
 // console.log("1" + 2)
 // console.log(1 + "2")
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// primitive 
+
+// 7 types : String , Number , Boolean , Null , Undefined , Symbol, BigInt
+
+
+
+// Non Premitive
+
+const score = 100
+const scoreValue = 100.00
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+// const bigNumber = 3457895463125789n
+
+// Reference (Non Premitive)
+
+// Array , Objects , Functions
+
+const heros = ["shaktiman" , "naagraj" , "Dora"];
+let myObj = {
+    name : "almas",
+    age : 27
+}
+
+
+const myFunction = function(){
+    console.log("Hello World")
+};
+
+console.log(typeof bigNumber);
