3fd770d2bf65acb46533f8a5b05142cadbc47f89
AlmasMehboobPatel <156776699+AlmasMehboobPatel@users.noreply.github.com>
2024-03-01 12:28:56 +0000

    strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,8 @@
+const name = "Almas Patel"
+const repoCount = "27"
+
+// console.log(`This is my name ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String ('AlmasPatel')
+// console.log(gameName[7]);
+console.log(gameName.__proto__);
